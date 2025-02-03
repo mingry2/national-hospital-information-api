e60c162aacee04c4d112764a8a32b23be3878052
minkyung kim <minry20415@naver.com>
2025-02-03 19:38:13 +0900

    ✨: init project

---
+++ src/main/java/com/mk/national_hospital_information/NationalHospitalInformationApplication.java
@@ -0,0 +1,13 @@
+package com.mk.national_hospital_information;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NationalHospitalInformationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NationalHospitalInformationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mk/national_hospital_information/NationalHospitalInformationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mk.national_hospital_information;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NationalHospitalInformationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
