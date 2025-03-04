f2d774e2a2a0fb012f82da023c0fa6c235d4db15
minkyung kim <minry20415@naver.com>
2025-03-04 10:53:12 +0900

    ♻️ refactor: DB insert 성능 최적화를 위한 리팩터링
    - ExcelService 코드 수정
    ㄴ XSSFWorkbook -> SXSSFWorkbook Stream 방식으로 변경
    ㄴ spring data JPA 사용이 아닌 EntityManager 직접 사용으로 변경
    ㄴ batchInsert 사용
    ㄴ flush(), clear()를 사용하여 캐시 비우기 BATCH_SIZE 만큼 DB INSERT 될 수 있게 변경
    - application.yml batch 설정 추가

--- src/main/java/com/mk/national_hospital_information/hospital/application/ExcelService.java
+++ src/main/java/com/mk/national_hospital_information/hospital/application/ExcelService.java
@@ -1,8 +1,9 @@
 package com.mk.national_hospital_information.hospital.application;
 
-import com.mk.national_hospital_information.hospital.application.interfaces.HospitalRepository;
 import com.mk.national_hospital_information.hospital.domain.Hospital;
 import com.mk.national_hospital_information.hospital.infrastructure.entity.HospitalEntity;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import java.io.BufferedInputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -13,7 +14,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.streaming.SXSSFWorkbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -23,22 +24,28 @@ import org.springframework.transaction.annotation.Transactional;
 @Slf4j
 public class ExcelService {
 
-    private final HospitalRepository hospitalRepository;
+//    private final HospitalRepository hospitalRepository;
+
+    @PersistenceContext
+    private EntityManager entityManager; // Spring Data JPA를 사용하지 않고 EntityManger를 직접 사용
+
+    private static final int BATCH_SIZE = 1000; // 배치크기 설정
 
     @Transactional
     public void saveHospitalsFromExcel(String filePath) {
 
         List<HospitalEntity> hospitalEntities = new ArrayList<>();
 
-        try (
-            FileInputStream fis = new FileInputStream(filePath);
+        try (FileInputStream fis = new FileInputStream(filePath);
             BufferedInputStream bis = new BufferedInputStream(fis);
-            Workbook workbook = new XSSFWorkbook(bis);
-        ) {
-            Sheet sheet = workbook.getSheetAt(0); // 첫번째 시트 읽기
+            XSSFWorkbook xssfWorkbook = new XSSFWorkbook(bis);
+            SXSSFWorkbook sxssfWorkbook = new SXSSFWorkbook(xssfWorkbook)) { // Streaming 방식으로 변경(메모리 사용량 줄이기)
+
+            Sheet sheet = sxssfWorkbook.getSheetAt(0); // 첫번째 시트 읽기
 
             for (Row row : sheet) {
-                if (row.getRowNum() == 0) continue;
+                if (row.getRowNum() == 0)
+                    continue; // 첫번째 행 스킵(컬럼명 행)
 
                 Hospital hospital = new Hospital(
                     getCellValue(row.getCell(1)),
@@ -49,19 +56,35 @@ public class ExcelService {
 
                 hospitalEntities.add(hospital.toEntity());
 
-                if (hospitalEntities.size() >= 1000) {
-                    hospitalRepository.saveAll(hospitalEntities);
-                    hospitalEntities.clear();
+                if (hospitalEntities.size() >= BATCH_SIZE) {
+                    batchInsert(hospitalEntities);
+                    hospitalEntities.clear(); // 리스트 초기화
                 }
-
-                if (!hospitalEntities.isEmpty()) {
-                    hospitalRepository.saveAll(hospitalEntities);
-                }
-
             }
+
+            // 남아 있는 데이터 저장
+            if (!hospitalEntities.isEmpty()) {
+                batchInsert(hospitalEntities);
+            }
+
         } catch (IOException e) {
             log.error("파일 읽기 오류", e);
         }
+    }
+
+    @Transactional // 전체 저장이 하나의 트랜잭션에서 이루어지도록 보장
+    public void batchInsert(List<HospitalEntity> hospitalEntities) {
+        for (int i = 0; i < hospitalEntities.size(); i++) {
+            entityManager.persist(hospitalEntities.get(i)); // 개별로 영속 상태로
+
+            if (i % BATCH_SIZE == 0) {
+                entityManager.flush(); // BATCH_SIZE 만큼 한번에 DB INSERT
+                entityManager.clear(); // 1차 캐시 비우기 (메모리 최적화)
+            }
+        }
+
+        entityManager.flush();
+        entityManager.clear();
     }
 
     private String getCellValue(Cell cell) {
